3fcaf936cbaeecadb0f3945ad65a25f8580a6688
sergiocuacor <2sergiocuadrado2000@gmail.com>
2024-10-02 14:12:52 +0200

    [ADD] proyecto1

---
+++ proyecto1/proyecto1/src/main/java/com/cbs/proyecto1/controllers/MainRestController.java
@@ -0,0 +1,13 @@
+package com.cbs.proyecto1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
