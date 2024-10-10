e8679e192027b62e9029072e305e23555b1f096f
sergiocuacorW <2sergiocuadrado2000@gmail.com>
2024-10-10 19:38:21 +0200

    [ADD] addManually endpoint - DNI verifier controller

--- proyectoUno/src/main/java/com/sergiocuacor/proyectoUno/controllers/StudentController.java
+++ proyectoUno/src/main/java/com/sergiocuacor/proyectoUno/controllers/StudentController.java
@@ -3,6 +3,9 @@ package com.sergiocuacor.proyectoUno.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sergiocuacor.proyectoUno.entities.Student;
 
@@ -37,10 +41,16 @@ public class StudentController {
 	}
 
 	@PostMapping("/addManually")
-	public List<Student> add1(@RequestBody Student student) {
+	public ResponseEntity<?> add1(@RequestBody Student student) {
+		for(Student s : studentList) {
+			if(student.getDni().equalsIgnoreCase(s.getDni())) {
+				return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: This student is already in the list.");
+			}
+		}
 		studentList.add(student);
-		return studentList;
+		return ResponseEntity.ok(student);
 	}
+	
 	
 	/* SI AL AÑADIR UN STUDENT YA EXISTE (POR DNI POR EJEMPLO), DEVOLVEMOS UN ERROR*/
 
